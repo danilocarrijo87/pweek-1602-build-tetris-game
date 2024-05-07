f11c59abe03afa38332dc5947c682ec4e3b01c80
LeelaManohar Masana <leelamanohar.masana@buildium.com>
2024-05-07 19:37:24 +0530

    Added the MainMenu unity

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadSceneAsync("SampleScene");
+        }
+    }
+}
