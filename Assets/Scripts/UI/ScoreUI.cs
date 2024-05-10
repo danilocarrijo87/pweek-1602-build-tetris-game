a05e2918520d053b97b468c2a90bb000fe2e2a61
Sathya Bhavani <sathya.bhavani@realpage.com>
2024-05-10 17:27:55 +0530

    PR for showing congrats msg for every 1000 points.

--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,8 @@ using UnityEngine;
 public class ScoreUI : MonoBehaviour
 {
     private TextMeshProUGUI _textMeshProUGUI;
+    public Transform textFlag;
+    public GameObject fireWorks;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +29,20 @@ public class ScoreUI : MonoBehaviour
     private void OnScoreChangeEvent(int i)
     {
         _textMeshProUGUI.text = i.ToString();
+        int intScore = Int32.Parse(_textMeshProUGUI.text);
+        if (intScore % 1000 == 0 && intScore!= 0)
+        { 
+            StartCoroutine(NextLevelMessage());
+        }
+        
     }
+    private IEnumerator NextLevelMessage()
+    {
+        textFlag.gameObject.SetActive(true);
+        fireWorks.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        textFlag.gameObject.SetActive(false);
+        fireWorks.SetActive(false);
+    }
+
 }
